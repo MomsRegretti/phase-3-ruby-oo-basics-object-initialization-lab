f3883136c6821448f0535b9d2cbc4d84a072a3e0
CharlieRojas <crc_185@yahoo.com>
2022-05-04 21:43:56 -0500

    commit tree 2789a5db1842d7fb98f99ab53eee4a075127844f

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+    attr_reader :name, :breed
+
+    def initialize(name, breed = "Mutt")
+        @breed = breed
+        @name = name
+    end
+    
+    lassie = Dog.new("Collie")
+    lassie.breed
+end
